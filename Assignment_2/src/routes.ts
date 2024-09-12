857533e37fecbbacc83aebdfcbfe36435c726882
Owais Dashti <owaisdashti@outlook.com>
2024-09-12 16:45:14 +0200

    assignmentNumber

--- Assignment_2/src/routes.ts
+++ Assignment_2/src/routes.ts
@@ -1,9 +1,14 @@
 import { Express, Request, Response } from "express";
 import UserRouter from "./routes/user.route";
 import TaskRouter from "./routes/task.route";
+import path from "path";
 
 function routes(app: Express) {
-  app.get("/", (req: Request, res: Response) => res.send("Hello from Assignment 1 in Software Quality!"));
+  const assignmentNumber = path.basename(process.cwd()).split("_")[1];
+
+  app.get("/", (req: Request, res: Response) =>
+    res.send(`Hello from Assignment ${assignmentNumber} in Software Quality!`)
+  );
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   // Register API routes
