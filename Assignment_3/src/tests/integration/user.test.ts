d7a7e865861391b2189e12f694c11ed37a50860a
Owais Dashti <owaisdashti@outlook.com>
2024-09-18 14:05:26 +0200

    added husky

--- Assignment_3/src/tests/integration/user.test.ts
+++ Assignment_3/src/tests/integration/user.test.ts
@@ -1,6 +1,6 @@
-import supertest from 'supertest'
-import prisma from '../../../prisma/client'
-import { app } from '../setup/setup'
+import supertest from 'supertest';
+import prisma from '../../../prisma/client';
+import { app } from '../setup/setup';
 
 describe('User', () => {
   test('should get a user by id', async () => {
@@ -11,39 +11,39 @@ describe('User', () => {
       password: 'pass',
       createdAt: '2021-09-01T00:00:00.000Z',
       updatedAt: '2021-09-01T00:00:00.000Z',
-    }
-    await prisma.users.create({ data: user })
+    };
+    await prisma.users.create({ data: user });
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...userWithoutPass } = user
+    const { password, ...userWithoutPass } = user;
 
     // Act
-    const response = await supertest(app).get(`/api/users/${user.id}`)
+    const response = await supertest(app).get(`/api/users/${user.id}`);
 
     // Assert
-    expect(response.status).toBe(200)
-    expect(response.body).toEqual(userWithoutPass)
-  })
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(userWithoutPass);
+  });
 
   test('should create a user', async () => {
     // Arrange
     const user = {
       name: 'John Doe',
       password: 'pass',
-    }
+    };
 
     // Act
-    const response = await supertest(app).post('/api/users').send(user)
+    const response = await supertest(app).post('/api/users').send(user);
 
     // Assert
-    expect(response.status).toBe(201)
+    expect(response.status).toBe(201);
     expect(response.body).toEqual({
       id: expect.any(String),
       name: user.name,
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
-    })
-  })
+    });
+  });
 
   test('should get all users', async () => {
     // Arrange
@@ -58,40 +58,40 @@ describe('User', () => {
         name: 'Jane Doe',
         password: 'pass',
       },
-    ]
+    ];
 
     await prisma.users.createMany({
       data: users,
-    })
+    });
 
     // Act
-    const response = await supertest(app).get('/api/users')
+    const response = await supertest(app).get('/api/users');
 
     // Assert
-    expect(response.status).toBe(200)
+    expect(response.status).toBe(200);
     expect(response.body).toEqual(
       expect.arrayContaining(
         users.map(user => {
           // eslint-disable-next-line @typescript-eslint/no-unused-vars
-          const { password, ...userWithoutPass } = user
+          const { password, ...userWithoutPass } = user;
           return {
             ...userWithoutPass,
             id: expect.any(String),
             createdAt: expect.any(String),
             updatedAt: expect.any(String),
-          }
+          };
         }),
       ),
-    )
-    expect(response.body).toHaveLength(users.length)
-  })
+    );
+    expect(response.body).toHaveLength(users.length);
+  });
 
   test('should throw error when getting a user by id that does not exist', async () => {
     // Act
-    const response = await supertest(app).get(`/api/users/999`)
+    const response = await supertest(app).get(`/api/users/999`);
 
     // Assert
-    expect(response.status).toBe(404)
-    expect(response.body).toEqual({ error: 'User not found' })
-  })
-})
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({ error: 'User not found' });
+  });
+});
